082d75c5453fe09e833079fd71d298c6922a7ee7
Marin Boone <150065021+MarinB-56@users.noreply.github.com>
2024-10-04 17:36:11 +0200

    Add trainer's details page to the app (show view)

--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -2,4 +2,9 @@ class TrainersController < ApplicationController
   def index
     @trainers = Trainer.all
   end
+
+  def show
+    @trainer = Trainer.find(params[:id])
+    @pokemons = @trainer.pokemons
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root to: "pages#home"
 
   resources :pokemons, only: [:show, :index]
-  resources :trainers, only: [:show, :index, :new, :create]
+  resources :trainers, only: [:show, :index]
 end
